eb3833772db6dcab6ca9a686c294c3a53abab55a
root <kukreti.ashutosh@gmail.com>
2016-01-12 20:02:10 +0530

    Signup Form Validation for Python

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(Form):
+	first_name = StringField('First Name', validators=[DataRequired("Please Enter First Name.")])
+	middle_name = StringField('Middle Name')
+	last_name = StringField('Last Name', validators=[DataRequired("Please Enter Last Name.")])
+	email = StringField('Email', validators=[DataRequired("Please Enter Email."), Email("Please Enter Valid Email Address.")])
+	password = PasswordField('Password', validators=[DataRequired("Password Field Can't be empty."), Length(min=6, message="Password must be 6 or more characters")])
+	submit = SubmitField('Sign Up')
