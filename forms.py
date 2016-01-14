49dc49d420887a34902a774f4805a58f05464719
root <kukreti.ashutosh@gmail.com>
2016-01-14 18:28:21 +0530

    Adding Home Page After Login

--- forms.py
+++ forms.py
@@ -6,6 +6,15 @@ class SignupForm(Form):
 	first_name = StringField('First Name', validators=[DataRequired("Please Enter First Name.")])
 	middle_name = StringField('Middle Name')
 	last_name = StringField('Last Name', validators=[DataRequired("Please Enter Last Name.")])
-	email = StringField('Email', validators=[DataRequired("Please Enter Email."), Email("Please Enter Valid Email Address.")])
+	email = StringField('Email', 
+		                 validators=[DataRequired("Please Enter Email."), 
+		                 Email("Please Enter Valid Email Address.")])
 	password = PasswordField('Password', validators=[DataRequired("Password Field Can't be empty."), Length(min=6, message="Password must be 6 or more characters")])
 	submit = SubmitField('Sign Up')
+
+class LoginForm(Form):
+	email = StringField('Email', 
+		                 validators=[DataRequired("Please Enter Email"), 
+		                 Email("Please Enter Valid Email Address")])
+	password = Password('Password', validators=[DataRequired("Please Enter Password.")])
+	submit = SubmitField('Sign in')
